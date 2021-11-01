049940396fb51062a073cfadea55bb1b4470c2d9
Pavel Petrov <doctorrosen2016@gmail.com>
2021-11-02 02:00:14 +0600

    added styles

--- src/components/CalendarGrid/CalendarGrid.jsx
+++ src/components/CalendarGrid/CalendarGrid.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import classes from "./CalendarGrid.module.css";
 
 const CalendarGrid = () => {
+    const totalDays = 35;
+    const daysArray = [...Array(totalDays)];
     return (
-        <div>
-            CalendarGrid
+        <div className={classes.wrapper}>
+            {daysArray.map((_, index) => (
+                <div className={classes.day} key={index}>
+                    <div className={classes.label}>{index}</div>
+                    <div className={classes.event}>{index}</div>
+                    <div className={classes.event}>{index}</div>
+                    <div className={classes.event}>{index}</div>
+                    <div className={classes.event}>{index}</div>
+                </div>
+            ))}
         </div>
     );
 };
