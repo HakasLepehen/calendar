f6f793af7169a3557fc1eccd7d972113945e64a5
Pavel Petrov <doctorrosen2016@gmail.com>
2021-11-08 14:08:29 +0600

    added some comments

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,21 +1,31 @@
 import './App.css';
+
+// Import self-made components
 import Month from "../Monitor/Month";
 import CalendarGrid from "../CalendarGrid/CalendarGrid";
 import Weekdays from "../WeekDays/WeekDays";
-import moment from "moment/min/moment-with-locales.min.js";
+
+import * as moment from "moment";
+import 'moment/locale/ru'; //Import moment.js and russian locale from library
+
 moment.locale('ru');
 
-let weekdays = moment.weekdays(true);
+let weekdays = moment.weekdays(true); //change weekdays in europe-like version
 let month = moment().format('MMMM');
 
+/**
+ * Main function of my application
+ **/
 function Index() {
+
+    // get range of days in this month
     const startDay = moment().startOf('month');
     const endDay = moment().endOf('month');
 
     return (
         <div className="App">
             <Month name={month}/>
-            <Weekdays week={weekdays} />
+            <Weekdays week={weekdays}/>
             <CalendarGrid startDay={startDay} endDay={endDay}/>
         </div>
     );
