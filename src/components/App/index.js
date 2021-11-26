372d4017e622fd205e0fc421afbe5edbf9d75cf6
Pavel Petrov <doctorrosen2016@gmail.com>
2021-11-26 16:51:34 +0600

    workig with routing

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import 'react-router';
+import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import Main from '../pages/Main.jsx';
 import Admin from '../pages/Admin';
 import Moderation from '../pages/Moderation';
@@ -18,9 +17,15 @@ const weekdays = moment.weekdays(true); // change weekdays in europe-like versio
  * Main function of my application
  */
 const Index = () => {
-  return (
-    <Router>
-      <div>
+  //тут я получаю извне данные по правам пользователя и задаю свои права на чтение и редактирование
+  const user = {
+    isAdmin: true,
+    isModerator: true,
+  };
+
+  if (user.isAdmin) {
+    return (
+      <Router>
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/moderation" element={<Moderation />} />
@@ -39,12 +44,33 @@ const Index = () => {
             </li>
           </ul>
         </nav>
+      </Router>
+    );
+  }
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-      </div>
-    </Router>
-  );
+  if (user.isModerator) {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/moderation" element={<Moderation />} />
+          <Route path="/admin" element={<div>У Вас нет доступа к этому URL</div>} />
+        </Routes>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">На главную</Link>
+            </li>
+            <li>
+              <Link to="/moderation">Изменить</Link>
+            </li>
+          </ul>
+        </nav>
+      </Router>
+    );
+  }
+
+  return <Main />;
 };
 
 export default Index;
