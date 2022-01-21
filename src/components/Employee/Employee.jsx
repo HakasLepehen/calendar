b21b1aec3938a737fad637f7db994affcf24cde2
Pavel Petrov <doctorrosen2016@gmail.com>
2022-01-21 17:58:30 +0600

    added count of shifts

--- src/components/Employee/Employee.jsx
+++ src/components/Employee/Employee.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import classes from './Employee.module.css';
 
 export default function Employee({ employee, selectingEmployee }) {
+  const shifts = useSelector((state) => state.shiftReducer.tempShifts);
+
+  const count = shifts.filter((shift) => shift.employee.id == employee.id).length;
   return (
     <label className={classes.employee}>
       <input
@@ -12,7 +16,7 @@ export default function Employee({ employee, selectingEmployee }) {
         onClick={selectingEmployee}
       />
       <span className={classes.employeeName}>{employee.name}</span>
-      <span className={classes.countShifts}>12</span>
+      <span className={classes.countShifts}>{count}</span>
     </label>
   );
 }
